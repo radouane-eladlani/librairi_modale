a447874e4c220968f7afba582bbc9ac45e32056a
radouane <radouane.eladlani@gmail.com>
2024-08-30 18:05:54 +0200

    modif css

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import './Modal.css';
 import React from 'react';
 
+
 const Modal = ({ isOpen, onClose, title, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className="modal-overlay" onClick={onClose}> {/* Overlay pour le fond transparent */}
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}> {/* Contenu de la modale */}
         <div className="modal-header">
           <h2>{title}</h2>
           <button onClick={onClose} className="close-button">
@@ -22,4 +23,3 @@ const Modal = ({ isOpen, onClose, title, children }) => {
 };
 
 export default Modal;
-
